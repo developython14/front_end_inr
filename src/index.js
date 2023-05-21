fbf23339bb8cc70a0e7cb3d3818654c921cdba98
developython14 <khasarou@gmail.com>
2023-05-21 03:58:22 +0100

    install required package

--- src/index.js
+++ src/index.js
@@ -3,36 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-
-
-
-
-const router = createBrowserRouter([ 
-  {
-    path: "/home",
-    element: <p>home</p>,
-  },
-  {
-    path: "/aboutus",
-    element: <p>about</p>,
-  },
-  {
-    path: "/redux",
-    element: <p>tredi</p>,
-  },
-
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider>
-      <RouterProvider router={router} />
-    </Provider>
+    <p>freee</p>
   </React.StrictMode>
 );
 
